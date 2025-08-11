6c0d2889e07b8670357424ca0aa1b4bc16a0cb9e
chutintorn.sri <zzz17244@gmail.com>
2025-08-11 16:47:26 +0700

    text top bar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,7 +4,7 @@ import { THEME } from "../config/theme";
 export default function TopBar({ lang, onToggleLang }) {
   return (
     <div className="border rounded-xl px-3 py-3 flex flex-wrap items-center gap-3 bg-[#ffc106]" style={{ borderColor: THEME.outline }}>
-      <div className="text-lg font-bold">Seat Selection</div>
+      <div className="text-lg font-bold">Seat & Privacy Selection</div>
       <div className="ml-auto flex gap-2">
         <button
           onClick={onToggleLang}
